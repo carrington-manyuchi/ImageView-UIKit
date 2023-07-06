9f4fc446910dd5cad061742bbd8daefc04f75574
Carrington <Manyuchi>
2023-07-06 12:24:33 +0200

    animation of images

--- ImageView-UIKit/ViewController.swift
+++ ImageView-UIKit/ViewController.swift
@@ -8,12 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private let myFirstImageView: UIImageView = {
+        let images = [ #imageLiteral(resourceName: "bird"), #imageLiteral(resourceName: "birds"), #imageLiteral(resourceName: "helicopter"), #imageLiteral(resourceName: "mother") ]
+        
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.animationImages = images
+        imageView.image = UIImage(named: "birds")
+        imageView.backgroundColor = .blue
+        imageView.animationDuration = 10
+        imageView.startAnimating()
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureView()
+        configureConstraints()
     }
 
+    private func configureView() {
+        view.addSubview(myFirstImageView)
+    }
 
+    private func configureConstraints() {
+        let myFirstImageViewConstraints = [
+            myFirstImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 50),
+            myFirstImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            myFirstImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 20),
+            myFirstImageView.heightAnchor.constraint(equalToConstant: 300)
+          
+        ]
+
+        NSLayoutConstraint.activate(myFirstImageViewConstraints)
+    }
 }
 
